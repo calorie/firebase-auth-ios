c162f3bd912e036468905a3973f2f44385e6b025
Yu Shigetani <s2g4t1n2@gmail.com>
2019-12-06 23:13:42 +0900

    Add proj

---
+++ firebase-auth-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  firebase-auth-ios
+//
+//  Created by yu on 2019/12/06.
+//  Copyright © 2019 example. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
