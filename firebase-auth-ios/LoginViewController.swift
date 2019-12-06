980fcc12ca273815264c070aa0e1ffc279a8d408
Yu Shigetani <s2g4t1n2@gmail.com>
2019-12-07 04:14:51 +0900

    Add login

---
+++ firebase-auth-ios/LoginViewController.swift
@@ -0,0 +1,15 @@
+//
+//  LoginViewController.swift
+//  firebase-auth-ios
+//
+//  Created by yu on 2019/12/07.
+//  Copyright © 2019 example. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
